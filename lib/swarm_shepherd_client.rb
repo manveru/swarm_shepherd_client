2aca6dca187340ae27f6dfdeeab074dc22ad576b
Michael Fellinger <m.fellinger@gmail.com>
2009-11-23 21:42:25 +0900

    remove engine

--- lib/swarm_shepherd_client.rb
+++ lib/swarm_shepherd_client.rb
@@ -3,6 +3,5 @@ require 'swarm_shepherd_common/couch'
 require 'swarm_shepherd_common/rubyrec'
 require 'swarm_shepherd_common/parts'
 require 'swarm_shepherd_common/envr'
-require 'swarm_shepherd_client/common'
-require 'swarm_shepherd_client/engine'
+
 require 'swarm_shepherd_client/client'
